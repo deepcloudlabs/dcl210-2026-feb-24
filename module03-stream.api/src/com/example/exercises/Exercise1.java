51b4228020c884ac94b8be5eb293ee8e53808055
Binnur KURT <deepcloudlabs@gmail.com>
2026-02-26 10:12:15 +0300

    initial commit

--- module03-stream.api/src/com/example/exercises/Exercise1.java
+++ module03-stream.api/src/com/example/exercises/Exercise1.java
@@ -1,7 +1,15 @@
 package com.example.exercises;
 
-import java.util.Collection;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.example.domain.Director;
 import com.example.domain.Movie;
 import com.example.service.InMemoryMovieService;
 import com.example.service.MovieService;
@@ -17,7 +25,34 @@ public class Exercise1 {
 	public static void main(String[] args) {
 		// Find the number of movies of each director
         final Collection<Movie> movies = movieService.findAllMovies();
-
+        // imperative programming: before java 8
+        System.err.println("Imperative Programming Style");
+        HashMap<Director,Long> directorMovieCount = new HashMap<>();
+        // external loop
+        for (var movie : movies) {
+           	// external loop
+        	    for (var director: movie.getDirectors()) {
+        	    		directorMovieCount.putIfAbsent(director, 0L);
+        	    		directorMovieCount.put(director, directorMovieCount.get(director)+1);
+        	    }
+        }
+        // external loop
+        for (var entry: directorMovieCount.entrySet()) {
+        		System.out.println("%-32s: %2d".formatted(entry.getKey().getName(),entry.getValue()));
+        }
+        System.err.println("Functional Programming Style");
+        // declarative programming: functional programming after java 8+
+        // Higher-Order Function: functions accept parameters as function
+        // MapReduce Framework: filter, map, reduce, flatMap, distinct, min, max, count, collect,...
+        // immutability, function chain (design): HoF -> pipeline (runtime): stream
+        Map<Director, Long> directorMovieFunctionalCount = 
+        // internal loop		
+        movies              							    // Collection<Movie> 
+              .stream()     								// Stream<Movie>
+              .map(Movie::getDirectors)                  // Stream<List<Director>>
+              .flatMap(List::stream)  				    // Stream<Director>
+              .collect(groupingBy(identity(),counting())); // Map<Director,Long>
+        directorMovieFunctionalCount.forEach((director,count) -> System.out.println("%-32s: %2d".formatted(director.getName(),count)));
 	}
 
 }
