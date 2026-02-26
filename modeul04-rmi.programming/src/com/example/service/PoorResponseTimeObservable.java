a67e1afd2cd7b5218b023744ffc17eb320d2c01a
Binnur KURT <deepcloudlabs@gmail.com>
2026-02-26 15:46:47 +0300

    Update PoorResponseTimeObservable.java

--- modeul04-rmi.programming/src/com/example/service/PoorResponseTimeObservable.java
+++ modeul04-rmi.programming/src/com/example/service/PoorResponseTimeObservable.java
@@ -2,6 +2,7 @@ package com.example.service;
 
 import java.util.Observable;
 
+@SuppressWarnings("deprecation")
 public class PoorResponseTimeObservable extends Observable {
 	public void changed() {
 		setChanged();
